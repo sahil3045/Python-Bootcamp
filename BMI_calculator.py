14cbc8d525609cf909a2994fff8c19c57b316fdb
sahil3045 <sahilkanchan3045@gmail.com>
2024-07-31 12:38:17 +0530

    Create BMI_calculator.py

---
+++ BMI_calculator.py
@@ -0,0 +1,19 @@
+print("Welcome to BMI calculator!!")
+
+height = float(input("Enter your height: "))
+
+weight = int(input("enter your weight: "))
+
+a = height*height
+
+BMI = weight/a
+if BMI < 18.5:
+    print("Your BMI is ", BMI, "you are underweight.")
+elif BMI < 25:
+    print("Your BMI is ", BMI, "you have a normal weight.")
+elif BMI < 30:
+    print("Your BMI is ", BMI, "you are slightly ooverweight.")
+elif BMI < 35:
+    print("Your BMI is ", BMI, "you are obese.")
+else:
+    print("Your BMI is ", BMI, "you are clinically obese.")
