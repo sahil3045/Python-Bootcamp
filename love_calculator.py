15347630b9ab8c4778675d47b96844e3e9e370c4
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-02 18:13:54 +0530

    Update love_calculator.py

---
+++ love_calculator.py
@@ -0,0 +1,17 @@
+print("The love calculator is calculating your score....")
+
+name1 = input("Enter name1: ")
+name2 = input("Enter name2: ")
+
+combined_names = name1 + name2
+a = combined_names.lower()
+t = a.count("t")
+r = a.count("r")
+r = a.count("u")
+r = a.count("e")
+
+l = a.count("l")
+o = a.count("o")
+v = a.count("v")
+e = a.count("e")
+
