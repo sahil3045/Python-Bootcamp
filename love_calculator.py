5f1c9284ac7ca9af40ce4ed518e40d2fd657b180
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-03 12:26:52 +0530

    Update love_calculator.py

--- love_calculator.py
+++ love_calculator.py
@@ -7,11 +7,20 @@ combined_names = name1 + name2
 a = combined_names.lower()
 t = a.count("t")
 r = a.count("r")
-r = a.count("u")
-r = a.count("e")
+u = a.count("u")
+e = a.count("e")
+first = t + r + u + e
 
 l = a.count("l")
 o = a.count("o")
 v = a.count("v")
 e = a.count("e")
+second = l + o + v + e
 
+score = (first) + (second)
+if (score < 10) or (score > 90):
+    print("Your score is ", score, " you go togeather like coke and mentos.")
+elif (score >= 40) or (score <= 50):
+    print("Your score is ", score, " you go alright together.")
+else: 
+    print("Your score is ", score, ".")
