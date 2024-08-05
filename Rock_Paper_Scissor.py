5e7904e20026f2bc3602660852210e992169764b
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-05 16:18:53 +0530

    Update Rock_Paper_Scissor.py

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -26,25 +26,31 @@ scissors = ("""
       (____)
 ---.__(___)
 """)
-
+images = [rock, paper, scissors]
 print("Welcome to rock paper scissors!!")
 a = int(input("Type 0 for rock, 1 for paper and 2 for scissors:\n"))
-comp = random.randint(0, 2)
-print(comp)
-
-if a == 0 and comp == 2:
-    print("You win!!")
-elif a == 2 and comp == 0:
-    print("You loose!")
-elif a ==2 and comp == 1:
-    print("You win!")
-elif a == 1 and comp == 0:
-    print("You win!")
-elif comp> a:
-    print("You loose!")
-elif a == comp:
-    print("Its a Draw.")
-else: 
+print("You chose: ")
+if a >= 3 or a < 0: 
     print("You typed an invalid number.")
+else:
+    print(images[a])
+    comp = random.randint(0, 2)
+    print("Computer chose: ")
+    print(images[comp])
+
+
+    if a == 0 and comp == 2:
+        print("You win!!")
+    elif a == 2 and comp == 0:
+        print("You loose!")
+    elif a ==2 and comp == 1:
+        print("You win!")
+    elif a == 1 and comp == 0:
+        print("You win!")
+    elif comp> a:
+        print("You loose!")
+    elif a == comp:
+        print("Its a Draw.")
+
 
 
