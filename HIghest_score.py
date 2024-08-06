113327f00c5012dea290d45cb122485f8d6f294f
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-06 21:51:53 +0530

    Create HIghest_score.py

---
+++ HIghest_score.py
@@ -0,0 +1,8 @@
+studentscores = [150, 198, 176, 134, 160, 123, 167, 182, 145, 139, 119]
+maxscore = 0
+
+for score in studentscores:
+    if score > maxscore:
+        maxscore = score
+
+print(maxscore)
