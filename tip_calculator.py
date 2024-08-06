15ee61d5d12ae103c080cba3cbe4b7c005551b55
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-06 21:37:36 +0530

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -6,9 +6,9 @@ b = int(input("How much tip would you like to give(percentage)? 10, 12, or 15?: 
 
 c = int(input("How many people to split the bill? "))
 
-d = b/100*a+a
+x = b/100*a+a
 
-e = d/c
+e = x/c
 round(e, 2)
 
 print("Each person should pay: $", e)
