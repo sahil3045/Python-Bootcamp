ea8bb905972f93bab6a3ad2d0322163aff6fb5d2
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-12 15:28:24 +0530

    Create Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,21 @@
+import random
+print("Welcome to Hangman game.")
+word_list = ["bottle", "phone", "pen", "pencil"]
+chosen_word = random.choice(word_list)
+
+guess = input("Guess the letter: ").lower()
+print(guess)
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
+
+
+
+
+
+
+
