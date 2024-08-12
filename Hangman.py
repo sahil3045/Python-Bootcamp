3f0a7a66b039849d9ff8b10636237dbec7520fb3
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-12 16:04:42 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -2,6 +2,12 @@ import random
 print("Welcome to Hangman game.")
 word_list = ["bottle", "phone", "pen", "pencil"]
 chosen_word = random.choice(word_list)
+len = len(chosen_word)
+
+placeholder = ""
+for position in range(0, len):
+    placeholder += "_"
+print(placeholder)
 
 guess = input("Guess the letter: ").lower()
 print(guess)
