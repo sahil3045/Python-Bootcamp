8c441692f7a30b2a6a00ea38f6fcb54dd4841555
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-16 18:33:27 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -50,7 +50,7 @@ stages = ['''
       |
 =========''']
 print("Welcome to Hangman game.")
-word_list = ["bottle", "phone", "pen", "pencil", "notes","book", ]
+word_list = ["bottle", "phone", "pen", "pencil", "notes", "book", "towel", "watch"]
 chosen_word = random.choice(word_list)
 len = len(chosen_word)
 
