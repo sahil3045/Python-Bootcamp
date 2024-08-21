e0ea59c891f2af929414767be7e3b6bbc79c3641
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-21 21:37:54 +0530

    Update Leap_year.py

--- Leap_year.py
+++ Leap_year.py
@@ -3,7 +3,7 @@ a = int(input("Enter the year: "))
 if a%4 == 0:
     if a%100 ==0:
         if a%400 == 0:
-            print("The year is leap year.")
+            print("Year is leap year.")
         else:
             print("Year is not a leap year.")
     else:
