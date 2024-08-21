a009f03f721323a6735b2024ada76af7c1954cda
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-21 21:38:46 +0530

    Update HIghest_score.py

--- HIghest_score.py
+++ HIghest_score.py
@@ -2,7 +2,7 @@ studentscores = [150, 198, 176, 134, 160, 123, 167, 182, 145, 139, 119]
 maxscore = 0
 
 for scores in studentscores:
-    if score > maxscore:
+    if scores > maxscore:
         maxscore = scores
 
 print(maxscore)
